0c48dfdd585938ae67b6b99e29ce2e2677677819
makkei0526 <makkei1710@gmail.com>
2020-04-03 20:41:25 +0900

    initial commit

---
+++ lib/kashima_view_tool.rb
@@ -0,0 +1,7 @@
+require "kashima_view_tool/version"
+require "kashima_view_tool/renderer"
+
+module KashimaViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/kashima_view_tool/version.rb
@@ -0,0 +1,3 @@
+module KashimaViewTool
+  VERSION = "0.1.0"
+end
